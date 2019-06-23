3a48a43bab020cf5d99e5868e36568cfa72e373d
Cyber Witch <no-reply>
2019-06-22 21:42:47 -0600

    started working on the App.js file and some of its logic.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import icons from "./icons.json"
+import Icon from "./components/Icon.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    score: 0,
+    highscore: 0,
+    icons: icons,
+    picked: []
+  }
+
+  shuffle = function(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+  onClick = (id) =>{
+
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Clicky Game</h1>
+
+        <div className="flex-container" id = "score-flex">
+          <p>Score: {this.state.score}</p>
+          <p>Highscore: {this.state.highscore}</p>
+        </div>
+        
+        <div>
+          {icons.map( icon => (
+
+            <Icon src={icon.src} id={icon.id} name={icon.name} onClick={this.onClick(icon)}/>
+          )
+           
+          )}}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Icon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function Icon(props){
+    
+
+    return(
+        <img src = {props.src} width = "200px" height = "200px" alt = {props.name}/>
+    )
+}
+
+export default Icon
